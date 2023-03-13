dd5d40b104e88cdd70e7bc85a0c284e9ee246e71
ikrokegb <ikrokegb@gmail.com>
2023-03-13 22:12:36 +0300

    fix DB error

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from 'mongoose'
+
+mongoose
+    .connect('mongodb+srv://ikristaai:66ggttrr45@cluster0.4aundnw.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err));
+
 
 const app = express();
 

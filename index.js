c083a8fd37e7bbdf05c2417d0978fd49dadfd5e0
ikrokegb <ikrokegb@gmail.com>
2023-03-17 21:09:51 +0300

    create userModel

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import { registerValidation } from './validations/auth.js';
+import { validationResult } from 'express-validator';
 
 mongoose
     .connect('mongodb+srv://ikristaai:66ggttrr45@cluster0.4aundnw.mongodb.net/?retryWrites=true&w=majority')
@@ -12,34 +14,9 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('hello world!');
+app.post('/auth/redister', registerValidation, (req, res) => {
+
 });
-
-app.post('/auth/login', (req, res) => {
-    console.log(req.body);
-
-let token = null;
-
-if (req.body.email === "test@test.ru") {
-    const token = jwt.sign(
-        {
-            email: req.body.email,
-            fullName: 'петька ушанкин',
-        },
-        'secret123',
-    );
-
-
-    res.json({
-        success: true,
-        token,
-    });
-
-}
-
-}
-)
 
 app.listen(4444, (err) => {
     if (err) {

111c10af4b9879c0c215b898c9598ea31c3d2966
ikrokegb <ikrokegb@gmail.com>
2023-03-17 21:57:00 +0300

    fix userModel error

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose';
 import { registerValidation } from './validations/auth.js';
 import { validationResult } from 'express-validator';
 
+import UserModel from './models/User.js';
+
 mongoose
     .connect('mongodb+srv://ikristaai:66ggttrr45@cluster0.4aundnw.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('DB ok'))
@@ -14,8 +16,22 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/redister', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
 
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.aratarUrl,
+        passwordHash: req.body.email,
+    });
+
+    res.json({
+        success: true,
+    });
 });
 
 app.listen(4444, (err) => {

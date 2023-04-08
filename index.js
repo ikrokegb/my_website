2e76ad7e0b32a50f13c781349271d2c49194764c
ikrokegb <ikrokegb@gmail.com>
2023-04-08 20:28:05 +0300

    fix cors error

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import multer from 'multer';
+import cors from 'cors';
 
 import mongoose from 'mongoose';
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 
-import checkAuth from './utils/checkAuth.js';
+import {handleValidationErrors, checkAuth} from './utils/index.js';
 
-import * as UserController from './controllers/UserController.js'
-import * as PostController from './controllers/PostController.js'
-import handleValidationErrors from './utils/handleValidationErrors.js';
+import { UserController, PostController } from './controllers/index.js';
 
 mongoose
-    .connect('mongodb+srv://ikristaai:66ggttrr45@cluster0.4aundnw.mongodb.net/blog?retryWrites=true&w=majority')
+    .connect(
+        'mongodb+srv://ikristaai:66ggttrr45@cluster0.4aundnw.mongodb.net/blog?retryWrites=true&w=majority'
+        )
     .then(() => console.log('DB ok'))
     .catch((err) => console.log('DB error', err));
 
@@ -30,6 +31,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
@@ -51,7 +53,8 @@ app.patch(
     checkAuth, 
     postCreateValidation, 
     handleValidationErrors,
-    PostController.update);
+    PostController.update,
+    );
 
 app.listen(4444, (err) => {
     if (err) {
